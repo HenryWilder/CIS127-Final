713f208ed969083f2b56fd79540c3618c1ab71f3
Henry Wilder <henrythepony@gmail.com>
2023-10-12 16:59:30 -0400

    Added serialization and started working on menu

--- CIS127-Final/Main.cpp
+++ CIS127-Final/Main.cpp
@@ -11,13 +11,96 @@
  * - Play must encounter other "characters"
  */
 #include <iostream>
+#include <string>
+#include <vector>
 #include "Serialize.h"
 using std::cin;
 using std::cout;
+using std::endl;
+using std::getline;
+using std::string;
+using std::vector;
+
+enum class MenuItem
+{
+    EXIT_GAME,
+    OPTIONS,
+    NEW_GAME,
+    CONTINUE,
+    LOAD_GAME,
+};
+
+MenuItem GetMainMenuSelection()
+{
+    const string MENU_ITEM_CONTINUE = "Continue";
+    const string MENU_ITEM_NEWGAME = "New game";
+    const string MENU_ITEM_OPTIONS = "Options";
+    const string MENU_ITEM_LOAD = "Load game";
+    const string MENU_ITEM_EXIT = "Exit game";
+    vector<string> menuOptions;
+    menuOptions.reserve(5);
+
+    if (config.SaveExists())
+    {
+        menuOptions = { MENU_ITEM_CONTINUE, MENU_ITEM_OPTIONS, MENU_ITEM_LOAD, MENU_ITEM_NEWGAME, MENU_ITEM_EXIT };
+    }
+    else
+    {
+        menuOptions = { MENU_ITEM_NEWGAME, MENU_ITEM_OPTIONS, MENU_ITEM_EXIT };
+    }
+
+    for (size_t i = 0; i < menuOptions.size(); ++i)
+    {
+        cout << (i + 1) << ": " << menuOptions[i] << '\n';
+    }
+
+    char menuItemSelectionNumber = '0';
+    while (menuItemSelectionNumber < '1' || '0' + menuOptions.size() < menuItemSelectionNumber)
+    {
+        cout << "Select a menu item: ";
+        cin >> menuItemSelectionNumber;
+    }
+    const string& selectedMenuItem = menuOptions[menuItemSelectionNumber - (size_t)'1'];
+    cout << "You selected " << selectedMenuItem << endl;
+
+    if (selectedMenuItem == MENU_ITEM_CONTINUE)
+    {
+
+    }
+    else if (selectedMenuItem == MENU_ITEM_EXIT)
+    {
+        return MenuItem::EXIT_GAME;
+    }
+}
 
 int main()
 {
-    // Todo
+    LoadConfigData();
+
+    bool shouldGameClose = false;
+
+    while (!shouldGameClose)
+    {
+        MenuItem selectedMenuItem = GetMainMenuSelection();
+        switch (selectedMenuItem)
+        {
+        case MenuItem::EXIT_GAME:
+            shouldGameClose = true;
+            break;
+
+        case MenuItem::OPTIONS:
+            break;
+
+        case MenuItem::NEW_GAME:
+            break;
+
+        case MenuItem::CONTINUE:
+            break;
+
+        case MenuItem::LOAD_GAME:
+            break;
+        }
+    }
 
     return 0;
 }
--- CIS127-Final/Serialize.h
+++ CIS127-Final/Serialize.h
@@ -1,9 +1,34 @@
 #pragma once
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
 
+// Minimum of 6 items
 struct SaveData
 {
-    // Todo: Add fields
-
-    void SaveToFile(const char* filename);
-    void LoadFromFile(const char* filename);
+    string playerName = "Ugene Plunderbuss";
+    // todo: player inventory
+    // todo: player room
 };
+
+struct ConfigData
+{
+    int currentSaveIndex = -1;
+    vector<string> saveNames = {};
+
+    bool SaveExists() const;
+    const string& CurrentSaveName() const;
+};
+
+constexpr const char* CONFIG_FILENAME = "config.txt";
+extern ConfigData config;
+
+// Saves data from from config to [CONFIG_FILENAME].
+void SaveConfigData();
+
+// Loads data from [CONFIG_FILENAME] to config.
+void LoadConfigData();
+
+bool Save(const string& filename, const SaveData& data);
+bool Load(const string& filename, SaveData& data);
