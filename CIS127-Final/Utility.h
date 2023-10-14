c001ebcbc0612cb9b8d6b830c2cac5aebe89443b
Henry Wilder <henrythepony@gmail.com>
2023-10-14 19:24:04 -0400

    Worked on constexpr math functions

--- CIS127-Final/Utility.h
+++ CIS127-Final/Utility.h
@@ -1,18 +1,126 @@
 #pragma once
 #include <concepts>
+#include <array>
+#include <limits>
+#include <sal.h>
+
+// The name scheme may seem a little weird. It kinda is.
+// I tried to keep in consistent in the sense that math uses snake_case, while higher level functions use PascalCase.
+// Imagine it as being where the snake_case is stuff I could imagine the standard library implementing,
+// while PascalCase stuff is what I would imagine Unity would have to implement on top of that.
+// (camalCase is used where the thing has an OpenGL parallel)
+
+template<typename _Ty>
+concept numeric = std::is_floating_point_v<_Ty> || std::is_integral_v<_Ty>;
+
+template<std::floating_point _Ty = float> constexpr _Ty PI = (_Ty)3.141592653589793238462643;
+template<std::floating_point _Ty = float> constexpr _Ty TAU = PI<_Ty> * 2; // 2pi
+
+template<std::floating_point _Ty>
+constexpr _Ty deg_to_rad(_Ty angle)
+{
+    return angle * TAU<_Ty>;
+}
+
+template<std::floating_point _Ty>
+constexpr _Ty rad_to_deg(_Ty angle)
+{
+    constexpr _Ty converter = 1 / TAU<_Ty>;
+    return angle * converter;
+}
+
+// constexpr math
+namespace cxmath
+{
+    template<numeric _Ty>
+    constexpr _Ty abs(_Ty x)
+    {
+        return x < 0 ? -x : x;
+    }
+    static_assert(abs(1) == 1);
+    static_assert(abs(-1) == 1);
+    static_assert(abs(0) == 0);
+
+    // Compiletime constexpr square root. Uses Newton Raphson method. Loops instead of using recursion.
+    // You are not meant to use this for negatives. But if you do, the result will be the imaginary number's coefficient.
+    // Infinity * infinity is still infinity, therefore the square root of infinity is negative infinity.
+    template<std::floating_point _Ty>
+    constexpr _Ty sqrt(_In_range_(>= , 0) _Ty x)
+    {
+        x = x >= 0 ? x : -x;
+
+        if (x == std::numeric_limits<_Ty>::infinity())
+        {
+            return std::numeric_limits<_Ty>::infinity();
+        }
+
+        _Ty curr = x;
+        _Ty prev = 0;
+        while (curr != prev)
+        {
+            prev = curr;
+            curr = (curr + x / curr) / 2;
+        }
+        return curr;
+    }
+    static_assert(sqrt(4.0f) == 2.0f);
+    static_assert(sqrt(16.0f) == 4.0f);
+    static_assert(sqrt(0.25f) == 0.5f);
+    static_assert(sqrt(0.0f) == 0.0f);
+    static_assert(sqrt(1.0f) == 1.0f);
+    static_assert(sqrt(-1.0f) == 1);
+    static_assert(sqrt(std::numeric_limits<float>::infinity()) == std::numeric_limits<float>::infinity());
+    static_assert(sqrt(-std::numeric_limits<float>::infinity()) == std::numeric_limits<float>::infinity());
+
+    template<std::floating_point _Ty>
+    constexpr _Ty fmod(_Ty numer, _Ty denom)
+    {
+        return (int)(numer / denom) * numer - denom;
+    }
+    static_assert(fmod(1.0f, 1.0f) == 0.0f);
+    static_assert(NearlyEqual(fmod(0.5f, 1.0f), 0.5f));
+    static_assert(fmod(0.75f, 0.5f) == 0.25f);
+    static_assert(fmod(-5.5f, 3.0f) == -2.5f);
+
+    // Compiletime constexpr sine.
+    template<std::floating_point _Ty>
+    constexpr _Ty sin(_Ty x)
+    {
+        _Ty t = x;
+        _Ty prev = 0;
+        _Ty sine = t;
+        int n = 1;
+        const _Ty xnegx = -x * x;
+        while (sine != prev)
+        {
+            prev = sine;
+            int n2 = 2 * n; ++n;
+            sine += t *= xnegx / ((n2 + 1) * n2);
+        }
+        return sine;
+    }
+    static_assert(sin(0.0f) == 0.0f);
+    static_assert(NearlyEqual(sin(PI<float> / 2), 1.0f));
+    static_assert(NearlyEqual(sin(PI<float>), 0.0f));
+}
+using namespace cxmath; // If std ever shadows these functions, they can still be accessed explicitly.
 
 // Epic! static assertions can test lambdas!!
 static_assert(([]() { return true; })());
 static_assert(([]() { return false; })() == false);
 
 template<typename _Ty>
-concept numeric = std::is_floating_point_v<_Ty> || std::is_integral_v<_Ty>;
-
-template<typename _Ty>
 concept comparible = requires(_Ty a, _Ty b)
 {
     { a < b } -> std::same_as<bool>;
 };
+
+template<std::floating_point _Ty>
+constexpr bool NearlyEqual(_Ty a, _Ty b, _Ty within = (_Ty)0.001) noexcept
+{
+    _Ty diff = a - b;
+    return -within <= diff && diff <= within;
+}
 
 // Keeps a number between two values
 template<comparible _Ty>
@@ -79,6 +187,65 @@ constexpr _Ty cerp(_Ty a, _Ty b, _Ty c, _Ty d, _TTy t) noexcept
     return qerp(lerp(a, b), lerp(b, c), lerp(c, d), t);
 }
 
+#if 0
+template<size_t _Size>
+class nbool
+{
+public:
+    nbool() = default;
+
+    template<std::same_as<bool> ..._Args, typename std::enable_if<sizeof...(_Args) == _Size, bool>::type = false>
+    constexpr nbool(_Args... args) noexcept
+    {
+        this->values = { args... };
+    }
+
+    constexpr nbool(bool fill)
+    {
+        for (bool& v : values)
+        {
+            v = fill;
+        }
+    }
+
+    constexpr bool& operator[](size_t index) { return values[index]; }
+    constexpr bool operator[](size_t index) const { return values[index]; }
+
+    constexpr nbool& operator&=(nbool other) noexcept { for (size_t i = 0; i < _Size; ++i) { values[i] = values[i] && other[i]; } return *this; }
+    constexpr nbool& operator|=(nbool other) noexcept { for (size_t i = 0; i < _Size; ++i) { values[i] = values[i] || other[i]; } return *this; }
+    constexpr nbool& operator^=(nbool other) noexcept { for (size_t i = 0; i < _Size; ++i) { values[i] = values[i] != other[i]; } return *this; }
+
+    constexpr nbool operator&(nbool other) const noexcept { nbool result = *this; result &= other; return result; }
+    constexpr nbool operator|(nbool other) const noexcept { nbool result = *this; result |= other; return result; }
+    constexpr nbool operator^(nbool other) const noexcept { nbool result = *this; result ^= other; return result; }
+    constexpr nbool operator~() const noexcept { nbool result = *this; for (size_t i = 0; i < _Size; ++i) { result[i] = !result[i]; } return result; }
+
+    constexpr bool operator==(nbool other) const noexcept { for (size_t i = 0; i < _Size; ++i) { if (values[i] != other[i]) return false; } return true; }
+    constexpr bool operator!=(nbool other) const noexcept { for (size_t i = 0; i < _Size; ++i) { if (values[i] != other[i]) return true; } return false; }
+
+    constexpr bool none() const noexcept { for (bool x : values) { if (x) return false; } return true; }
+    constexpr bool all() const noexcept { for (bool x : values) { if (!x) return false; } return true; }
+    constexpr bool any() const noexcept { for (bool x : values) { if (x) return true; } return false; }
+
+    constexpr size_t size() const noexcept { return _Size; }
+
+private:
+    std::array<bool, _Size> values;
+};
+using bool2 = nbool<2>;
+using bool3 = nbool<3>;
+
+static_assert(bool2(false, true)[0] == false);
+static_assert(bool2(false, true)[1] == true);
+
+static_assert(!nbool<4>(true, true, false, true).all());
+static_assert(nbool<4>(true, true, true, true).all());
+static_assert(nbool<4>(false, false, true, false).any());
+static_assert(!nbool<4>(false, false, false, false).any());
+static_assert(nbool<4>(false, false, false, false).none());
+static_assert(!nbool<4>(false, false, true, false).none());
+#endif
+
 
 struct ivec2
 {
@@ -92,13 +259,13 @@ struct ivec2
 
     int x, y;
 
-    static constexpr ivec2 zero()     { return ivec2(+0); }
-    static constexpr ivec2 one()      { return ivec2(+1); }
-    static constexpr ivec2 negative() { return ivec2(-1); }
-    static constexpr ivec2 right()    { return ivec2(+1,+0); }
-    static constexpr ivec2 up()       { return ivec2(+0,+1); }
-    static constexpr ivec2 left()     { return ivec2(-1,+0); }
-    static constexpr ivec2 down()     { return ivec2(+0,-1); }
+    static constexpr ivec2 zero()     noexcept { return ivec2(+0); }
+    static constexpr ivec2 one()      noexcept { return ivec2(+1); }
+    static constexpr ivec2 negative() noexcept { return ivec2(-1); }
+    static constexpr ivec2 right()    noexcept { return ivec2(+1,+0); }
+    static constexpr ivec2 up()       noexcept { return ivec2(+0,+1); }
+    static constexpr ivec2 left()     noexcept { return ivec2(-1,+0); }
+    static constexpr ivec2 down()     noexcept { return ivec2(+0,-1); }
 
     constexpr ivec2 operator+() const { return *this; }
     constexpr ivec2 operator-() const { return ivec2(-x, -y); }
@@ -142,22 +309,13 @@ struct vec2
 
     float x, y;
 
-    static constexpr vec2 zero()     { return vec2(+0.0f); }
-    static constexpr vec2 one()      { return vec2(+1.0f); }
-    static constexpr vec2 negative() { return vec2(-1.0f); }
-    static constexpr vec2 right()    { return vec2(+1.0f, +0.0f); }
-    static constexpr vec2 up()       { return vec2(+0.0f, +1.0f); }
-    static constexpr vec2 left()     { return vec2(-1.0f, +0.0f); }
-    static constexpr vec2 down()     { return vec2(+0.0f, -1.0f); }
-
-    constexpr bool NearlyEqual(vec2 other, float within = 0.001f) const
-    {
-        float xDiff = x - other.x;
-        float yDiff = y - other.y;
-        return
-            (-within <= xDiff && xDiff <= within) &&
-            (-within <= yDiff && yDiff <= within);
-    }
+    static constexpr vec2 zero()     noexcept { return vec2(+0.0f); }
+    static constexpr vec2 one()      noexcept { return vec2(+1.0f); }
+    static constexpr vec2 negative() noexcept { return vec2(-1.0f); }
+    static constexpr vec2 right()    noexcept { return vec2(+1.0f, +0.0f); }
+    static constexpr vec2 up()       noexcept { return vec2(+0.0f, +1.0f); }
+    static constexpr vec2 left()     noexcept { return vec2(-1.0f, +0.0f); }
+    static constexpr vec2 down()     noexcept { return vec2(+0.0f, -1.0f); }
 
     constexpr vec2 operator+() const { return *this; }
     constexpr vec2 operator-() const { return vec2(-x, -y); }
@@ -184,10 +342,32 @@ struct vec2
 
     constexpr bool operator==(vec2 other) { return x == other.x && y == other.y; }
     constexpr bool operator!=(vec2 other) { return x != other.x || y != other.y; }
+
+    constexpr float lengthSqr() const { return x * x + y * y; }
+
+    // Magnitude
+    constexpr float length() const { return sqrt(lengthSqr()); }
+
+    // Direction
+    constexpr vec2 normal() const { return *this / length(); }
+
+    // angle should be in radians
+    constexpr vec2 FromAngle(float angle) const { return vec2(cos(angle), sin(angle)); }
 };
 
-static_assert(lerp(vec2(0.0f), vec2(1.0f), 0.5f).NearlyEqual(vec2(0.5f)));
-static_assert(lerp(vec2(-1.0f, 1.0f), vec2(1.0f, 2.0f), 0.5f).NearlyEqual(vec2(0.0f, 1.5f)));
+static constexpr float dot(vec2 a, vec2 b) { return a.x * b.x + a.y + b.y; }
+static constexpr float det(vec2 a, vec2 b) { return a.x * b.y - a.y + b.x; }
+static constexpr float distanceSqr(vec2 a, vec2 b) { return ; }
+static constexpr float distance(vec2 a, vec2 b) { return ; }
+static constexpr float direction(vec2 a, vec2 b) { return ; }
+
+constexpr bool NearlyEqual(vec2 a, vec2 b, float within = 0.001f) noexcept
+{
+    return NearlyEqual(a.x, b.x, within) && NearlyEqual(a.y, b.y, within);
+}
+
+static_assert(NearlyEqual(lerp(vec2(0.0f), vec2(1.0f), 0.5f), vec2(0.5f)));
+static_assert(NearlyEqual(lerp(vec2(-1.0f, 1.0f), vec2(1.0f, 2.0f), 0.5f), vec2(0.0f, 1.5f)));
 
 
 struct vec3
@@ -208,26 +388,15 @@ struct vec3
 
     float x, y, z;
 
-    static constexpr vec3 zero()     { return vec3(+0.0f); }
-    static constexpr vec3 one()      { return vec3(+1.0f); }
-    static constexpr vec3 negative() { return vec3(-1.0f); }
-    static constexpr vec3 right()    { return vec3(+1.0f, +0.0f, +0.0f); }
-    static constexpr vec3 up()       { return vec3(+0.0f, +1.0f, +0.0f); }
-    static constexpr vec3 forward()  { return vec3(+0.0f, +0.0f, +1.0f); }
-    static constexpr vec3 left()     { return vec3(-1.0f, +0.0f, +0.0f); }
-    static constexpr vec3 down()     { return vec3(+0.0f, -1.0f, +0.0f); }
-    static constexpr vec3 backward() { return vec3(+0.0f, +0.0f, -1.0f); }
-
-    static constexpr bool NearlyEqual(vec3 a, vec3 b, float within = 0.001f)
-    {
-        float xDiff = a.x - b.x;
-        float yDiff = a.y - b.y;
-        float zDiff = a.z - b.z;
-        return
-            (-within <= xDiff && xDiff <= within) &&
-            (-within <= yDiff && yDiff <= within) &&
-            (-within <= zDiff && zDiff <= within);
-    }
+    static constexpr vec3 zero()     noexcept { return vec3(+0.0f); }
+    static constexpr vec3 one()      noexcept { return vec3(+1.0f); }
+    static constexpr vec3 negative() noexcept { return vec3(-1.0f); }
+    static constexpr vec3 right()    noexcept { return vec3(+1.0f, +0.0f, +0.0f); }
+    static constexpr vec3 up()       noexcept { return vec3(+0.0f, +1.0f, +0.0f); }
+    static constexpr vec3 forward()  noexcept { return vec3(+0.0f, +0.0f, +1.0f); }
+    static constexpr vec3 left()     noexcept { return vec3(-1.0f, +0.0f, +0.0f); }
+    static constexpr vec3 down()     noexcept { return vec3(+0.0f, -1.0f, +0.0f); }
+    static constexpr vec3 backward() noexcept { return vec3(+0.0f, +0.0f, -1.0f); }
 
     constexpr vec3 operator+() const { return *this; }
     constexpr vec3 operator-() const { return vec3(-x, -y, -z); }
@@ -254,7 +423,27 @@ struct vec3
 
     constexpr bool operator==(vec3 other) const { return x == other.x && y == other.y && z == other.z; }
     constexpr bool operator!=(vec3 other) const { return x != other.x || y != other.y || z != other.z; }
+
+    constexpr float lengthSqr() const { return x * x + y * y + z * z; }
+
+    // Magnitude
+    constexpr float length() const { return sqrt(lengthSqr()); }
+
+    // Direction
+    constexpr vec3 normal() const { return *this / length(); }
 };
+
+constexpr float dot(vec3 a, vec3 b) { return a.x * b.x + a.y + b.y + a.z * b.z; }
+constexpr float distanceSqr(vec3 a, vec3 b) {}
+constexpr float distance(vec3 a, vec3 b) { return sqrt(distanceSqr(a, b)); }
+constexpr vec3 direction(vec3 a, vec3 b) { return (b - a).normal(); }
+
+constexpr vec3 cross(vec3 a, vec3 b) { return (b - a).normal(); }
+
+constexpr bool NearlyEqual(vec3 a, vec3 b, float within = 0.001f) noexcept
+{
+    return NearlyEqual(a.x, b.x, within) && NearlyEqual(a.y, b.y, within) && NearlyEqual(a.z, b.z, within);
+}
 
 static_assert(([]() {
         vec3 v = vec3(2.0f);
@@ -293,44 +482,114 @@ struct Color
 
     byte r, g, b;
 
-    explicit constexpr operator vec3() const
+    explicit constexpr operator vec3() const noexcept
     {
-        constexpr float conversion = 1.0f / 255;
+        constexpr float conversion = 1.0f / 255.0f;
         return vec3(r * conversion, g * conversion, b * conversion);
     }
 };
 
 // Color equivalent of lerp
 template<can_interpolate<vec3> _Ty>
-constexpr Color mix(Color a, Color b, _Ty t)
+constexpr Color mix(Color a, Color b, _Ty t) noexcept
 {
     return Color(lerp((vec3)a, (vec3)b, t));
 }
 
-// A square is a rect with width == height
 struct irect
 {
+    irect() = default;
+
+    constexpr irect(int xmin, int ymin, int xmax, int ymax) :
+        xmin(xmin), ymin(ymin), xmax(xmax), ymax(ymax) {}
+
     int xmin, ymin, xmax, ymax;
+
+    static constexpr irect FromMinAndMax(ivec2 min, ivec2 max) noexcept
+    {
+        return irect(min.x, min.y, max.x, max.y);
+    }
+
+    static constexpr irect FromPositionAndSize(ivec2 pos, ivec2 size) noexcept
+    {
+        return irect(pos.x, pos.y, pos.x + size.x, pos.y + size.y);
+    }
+
+    static constexpr irect FromPositionAndSize(ivec2 pos, int size) noexcept
+    {
+        return irect(pos.x, pos.y, pos.x + size, pos.y + size);
+    }
 };
 
-// A square is a rect with width == height
 struct rect
 {
+    rect() = default;
+
+    constexpr rect(float x, float y, float size) :
+        xmin(x), ymin(y), xmax(x + size), ymax(y + size) {}
+
+    constexpr rect(vec2 position, float size) :
+        xmin(position.x), ymin(position.y), xmax(position.x + size), ymax(position.y + size) {}
+
+    constexpr rect(float xmin, float ymin, float xmax, float ymax) :
+        xmin(xmin), ymin(ymin), xmax(xmax), ymax(ymax) {}
+
     float xmin, ymin, xmax, ymax;
+
+    static constexpr irect FromMinAndMax(vec2 min, vec2 max) noexcept
+    {
+        return irect(min.x, min.y, max.x, max.y);
+    }
+
+    static constexpr irect FromPositionAndSize(vec2 pos, vec2 size) noexcept
+    {
+        return irect(pos.x, pos.y, pos.x + size.x, pos.y + size.y);
+    }
+
+    static constexpr irect FromPositionAndSize(vec2 pos, float size) noexcept
+    {
+        return irect(pos.x, pos.y, pos.x + size, pos.y + size);
+    }
 };
 
 struct triangle
 {
+    triangle() = default;
 
+    constexpr triangle(vec2 p1, vec2 p2, vec2 p3) :
+        points{ p1, p2, p3 } {}
+
+    vec2 points[3];
 };
 
 struct quad
 {
+    quad() = default;
 
+    constexpr quad(vec2 p1, vec2 p2, vec2 p3, vec2 p4) :
+        points{ p1, p2, p3, p4 } {}
+
+    vec2 points[4];
 };
 
-// A circle is an oval with rad1 == rad2
+struct circle
+{
+    circle() = default;
+
+    constexpr circle(vec2 position, float radius) :
+        position(position), radius(radius) {}
+
+    vec2 position;
+    float radius;
+};
+
 struct oval
 {
+    oval() = default;
 
+    constexpr oval(vec2 position, float r1, float r2) :
+        position(position), r1(r1), r2(r2) {}
+
+    vec2 position;
+    float r1, r2;
 };
